5cafd9bb59b5438dd5d3572bdb51824eb1d36680
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-01-10 09:56:28 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student student = new Student();
+        student.setName("Thắng");
+        student.setClasses("C1121G1");
+        System.out.println(student);
+    }
+}
---
+++ src/com/codegym/Student.java
@@ -0,0 +1,29 @@
+package com.codegym;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+    public String toString(){
+        return this.name + ", "+this.classes;
+    }
+}
